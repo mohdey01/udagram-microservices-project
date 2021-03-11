8fb890162f1cdf36a2a99a6b618cebd4440c7542
mohdey01 <mohinipandey179@gmail.com>
2021-03-11 13:29:20 +0530

    added tavis file

--- udagram-c3-deployment/docker/set_env.sh
+++ udagram-c3-deployment/docker/set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mohinipandey
-export POSTGRES_HOST=udagrampandeydev.csnv3w58gpjd.us-east-2.rds.amazonaws.com
-export POSTGRES_DB=udagrampandeydev
-export AWS_BUCKET=mohini-507346083625-dev
-export AWS_REGION=us-east-2
-export AWS_PROFILE=default
+export POSTGRES_USERNAME=
+export POSTGRES_PASSWORD=
+export POSTGRES_HOST=
+export POSTGRES_DB=
+export AWS_BUCKET=
+export AWS_REGION=
+export AWS_PROFILE=
 export JWT_SECRET=null
 export URL=http://localhost:8100
