eec17e8a86dc5bd08f37dd163abc1d5625344c31
mohdey01 <mohinipandey179@gmail.com>
2021-03-11 13:15:59 +0530

    microservices changes

---
+++ udagram-c3-deployment/docker/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=mohinipandey
+export POSTGRES_HOST=udagrampandeydev.csnv3w58gpjd.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=udagrampandeydev
+export AWS_BUCKET=mohini-507346083625-dev
+export AWS_REGION=us-east-2
+export AWS_PROFILE=default
+export JWT_SECRET=null
+export URL=http://localhost:8100
